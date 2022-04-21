75489d3386ccd7950d68b388d03ac7dceca69e78
Saori <pinkokinawa84@gmail.com>
2022-04-20 18:52:36 -0700

    ThighTooltip is activate when the thigh collide with Epi orange tip

---
+++ EpiTrainerHT/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public GameObject ThighTooltip;
+    void OnCollisionEnter(Collision collision)
+    {
+     
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Dummy")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            ThighTooltip.SetActive(true);
+            Debug.Log("Do something");
+            
+        }
+    }
+
+}
