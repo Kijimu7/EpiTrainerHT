d41d6c24e271fe6f00d4233714053ab03d163d1c
Saori <pinkokinawa84@gmail.com>
2022-04-21 12:19:01 -0700

    Added setactive = true for countdown timer

--- EpiTrainerHT/Assets/Scripts/CollisionManager.cs
+++ EpiTrainerHT/Assets/Scripts/CollisionManager.cs
@@ -8,11 +8,17 @@ public class CollisionManager : MonoBehaviour
     public GameObject TipTooltip;
     private AudioSource audioSource;
     private AudioClip ButtonUp;
+    public Canvas canvas; 
+    public CountDownTimer contDownTimer; //getting reference from CountDownTimer class
+   
  
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+       
+        GameObject tempObject = GameObject.Find("Canvas");
+        contDownTimer = tempObject.GetComponent<CountDownTimer>();
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -27,12 +33,13 @@ public class CollisionManager : MonoBehaviour
             Rigidbody tipRidgidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 forceDirection = collision.gameObject.transform.position;
              tipRidgidbody.AddForce(forceDirection * 10, ForceMode.Force);
-            
-            
+             
              audioSource.Play();
-            
+             contDownTimer.enabled = true;
+
 
         }
+        
     }
 
 }
---
+++ EpiTrainerHT/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 3f;
+
+    [SerializeField] TextMeshProUGUI countdownText;
+
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if(currentTime <=0)
+        {
+            currentTime = 0;
+        }
+    }
+}
